cdd5007bf39aa4415eefbda80e2a6e3f9e5fec55
Smriti Mishra <smriti@valuebound.com>
2024-09-17 17:21:26 +0530

    initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createProfile, updateProfile, deleteProfile} = require('../Controllers/authController');
+const {createProfile, updateProfile, deleteProfile} = require('../controllers/authController');
 
 
 
